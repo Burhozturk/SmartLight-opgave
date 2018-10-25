15eccda66bd0bc00faed96bdca51e4a8605df3c5
KugelbergDK <kugelberg.dk@gmail.com>
2018-10-25 09:49:28 +0200

    Added data members, constructor and methods

--- src/hu/hungarian/SmartLight.java
+++ src/hu/hungarian/SmartLight.java
@@ -4,6 +4,43 @@ import javafx.scene.paint.Color;
 
 public class SmartLight {
 
+    private short lightPercent;
+    private Color color;
+
+    public SmartLight(){
+
+        // Sluk for lampen
+        lightPercent = 0;
+
+        // Set color to white
+        color = Color.WHITESMOKE;
+    }
+
+    public void turnUp(){
+
+        if (lightPercent+20 >= 100){
+            lightPercent = 100;
+        }
+        else{
+            lightPercent += 20;
+        }
+
+    }
+
+    public void turnDown(){
+        if(lightPercent-20<=0)
+        {
+            lightPercent=0;
+        }
+        else
+        {
+            lightPercent-=20;
+        }
+    }
+
+    public String toString(){
+        return "";
+    }
 
 
 }
